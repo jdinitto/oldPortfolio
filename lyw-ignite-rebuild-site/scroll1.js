cf5c3953889c8b486986fe325d4023da6afa2d6b
Jay DiNitto <jay@jaydinitto.com>
2018-06-13 05:50:24 -0400

    add all files

---
+++ lyw-ignite-rebuild-site/scroll1.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var speed=3
+
+iens6=document.all||document.getElementById
+ns4=document.layers
+
+if (iens6){
+document.write('<div id="container" style="position:relative;width:496px;height:354px;overflow:hidden;border:0px ridge white">')
+document.write('<div id="content" style="position:absolute;width:496px;left:0px;top:0px">')
+}
+
---
+++ lyw-waking-giants-site/scroll1.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var speed=8
+
+iens6=document.all||document.getElementById
+ns4=document.layers
+
+if (iens6){
+document.write('<div id="container" style="position:relative;width:295px;height:461px;overflow:hidden;border:0px ridge white">')
+document.write('<div id="content" style="position:absolute;width:295px;left:0px;top:0px">')
+}
+
